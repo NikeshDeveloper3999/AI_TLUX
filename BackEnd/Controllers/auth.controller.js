71d08439e902cbf5e9c92dfcf95ba15dcbebcf0f
NikeshDeveloper3999 <nikeshparte726@gmail.com>
2026-06-19 04:07:03 +0530

    'add'

--- BackEnd/Controllers/auth.controller.js
+++ BackEnd/Controllers/auth.controller.js
@@ -10,12 +10,12 @@ export const googleAuth = async (req,res) => {
         let user = await User.findOne({email})
         if(!user){user = await User.create({name , email})}
         const token = await genToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        } )
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        })
 
         return res.status(200).json(user)
 
